8af2481142f8e42c0c45202db53dc6a6cdb7a354
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2023-09-03 23:02:37 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredientText) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredientText;
+  listItem.classList.add('item');
+  fragment.appendChild(listItem);
+});
+
+ingredientsList.appendChild(fragment);
