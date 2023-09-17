934437d2a331c750a6440852b8228534078e9b34
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2023-09-17 22:16:45 +0300

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
+const formElement = document.querySelector('.login-form')
 
+formElement.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const formData = new FormData(formElement)
+    
+    let filledForm = true
+    formData.forEach(field => { if (!field) filledForm = false })
+    
+    if (!filledForm) alert("Не все поля заполнены")
+    else {
+        const dataObject = {}
+        formData.forEach((value, index) => { dataObject[index] = value })
+        
+        console.log(dataObject)
+        formElement.reset()
+    }
+})
